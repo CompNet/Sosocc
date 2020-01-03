7b4ddb0fad29311853fa8efa50a3be1171e5616e
arinik9 <arinik9@hotmail.com>
2020-01-03 18:01:25 +0100

    add some remarks into main.R

--- src/main.R
+++ src/main.R
@@ -1,9 +1,14 @@
 
-
-# ===============================================================
-#
-# set CPLEX.BIN.PATH in src/define-algos.R
-# ===============================================================
+# =============================================================================
+# VARIABLES
+#   ==> REmark 1: Do not forget to set CPLEX.BIN.PATH correclty in src/define-algos.R
+#   ==> Remark 2: PROP.NEGS should be set to 'NA' when DENSITY=1.0
+#   ==> Remark 3: CORE.PART.THRESHOLD should be set to 1. Because, when it is less than 1,
+#                  there might be multiple way of building core part.
+#   ==> Remark 4: It is the responsability of the user who will ensure if the RAM requirement
+#                  of his/her system is ok for large graphs, because Cplex may require 
+#                  lots of RAM for graphs whose size is larger than 28.
+# =============================================================================
 
 
 ## libraries for parallel processing
@@ -13,7 +18,7 @@
 source("src/define-imports.R")
 
 
-plot.layout <- c(
+plot.layout <- c( # ==========> it is not taken into account everywhere !! TODO
     # "kamada.kawai"
     # "fruchterman.reingold"
     # "bn_zheng"
@@ -21,13 +26,13 @@ plot.layout <- c(
 )
 
 #####################
-plot.format <- c(
+plot.format <- c( # ==========> it is not taken into account everywhere !! TODO
     #PLOT.AS.PDF
     #PLOT.AS.JPEG
     #PLOT.AS.PNG
     JUST.PLOT
 )
-#COMPRESS=FALSE
+
 FORCE = FALSE
 
 keep.algo.log.files = FALSE
@@ -38,9 +43,9 @@ COR.CLU.EXACT.ALGO = get.ExCC.code(enum.all=TRUE)   # for all optimal solutions
 
 # not all the measures are distance measure, 
 # but we will convert all similarity measure into distance in the function 'compare.partition.pair'
-# IMPORTANT: if you want to calculate VI and EDIT distances, which are not normalized measures,
+# IMPORTANT: if you want to calculate EDIT distance, which is not normalized measures,
 #           and if you want to compute both the unnormalized and normalized versions:
-#           So, write first the unnormalized version, then add the normalized version. For instance, "VI", "NVI"
+#           So, write first the unnormalized version, then add the normalized version.
 #           if you want to use other distance measures, ordering is not important.
 #           We do this, because calculating EDIT distance might be time consuming, so its normalization should not be
 COMP.MEASURES <- c(
@@ -49,39 +54,26 @@ COMP.MEASURES <- c(
 #		EDIT # edit distance
 #		EDIT.NORM # normalized edit distance
 )
-# TODO: in compare-partitions.R, handle better the global variables in clusters
 
-# Note that we use VI to get the closest solution association between ExCC optimal solutions and heuristic solutions. 
-# Actually, what we want is just to know if a heuristic solution is optimal or not. so, distance is 0 or not..
 
-#
 #K.LIMITS = c(3,3) # LIMIT K-MEDOIDS RESULTS
 K.LIMITS = c(NA,NA) # LIMIT K-MEDOIDS RESULTS
 
 
-# #GRAPH.SIZES = seq(16, 36, by=4)
-# GRAPH.SIZES = c(20, 24)
-# DENSITY = 1
-# #K = 2
-# #K = 3
-# K = 4
-
 GRAPH.SIZES = c(20,24)
-
-#K = 2
-#K = 3
-K = 4
-
+L0 = 4
 PROP.MISPLS = c( seq(0.05, 0.20, by=0.05))
-
-#PROP.MISPLS = c( seq(0.05, 1.00, by=0.05))
-
-#PROP.MISPLS = c( seq(0.05, 0.40, by=0.05)) # k_init=4
-#PROP.MISPLS = c( seq(0.05, 0.60, by=0.05)) # k_init=3
-#PROP.MISPLS = c( seq(0.05, 0.85, by=0.05) ) # k_init=2
+DENSITY = 0.125 # graph density: we can only give a single value (not a vector !!)
+INPUT.RANDOM.NETWORKS = seq(1, 2, by=1)
+# ----------------------------------------------
+PROP.NEGS = c(0.2, 0.3)
+# Note that PROP.NEGS should be 'NA' when DENSITY=1.0
+# In the point of view of a developer, if PROP.NEGS = NA, we should know that prop.neg can be only 1 value and it can be computed from (graph.size, prop.mispl)
+# Otherwise, user should be able to give a common range of prop.negs for each pair of (graph.size, prop.mispl)
+# ----------------------------------------------
 
 DETECTED.IMB.INTERVALS = c()
-DETECTED.IMB.INTERVAL.SEQ.VALS = seq(0.00, 1.00, 0.05)
+DETECTED.IMB.INTERVAL.SEQ.VALS = seq(0.00, 1.00, 0.05) # tis vector does not matter. In any case, we check if the folder exists or not
 for(i in 1:(length(DETECTED.IMB.INTERVAL.SEQ.VALS)-1)){
     lower.bound = DETECTED.IMB.INTERVAL.SEQ.VALS[i]
     upper.bound = DETECTED.IMB.INTERVAL.SEQ.VALS[i+1]
@@ -90,99 +82,72 @@ for(i in 1:(length(DETECTED.IMB.INTERVAL.SEQ.VALS)-1)){
 }
 
 
-# graph density
-DENSITY = 0.125 # 0.250 ====> we can only give a single value (not a vector !!)
 
-
-# ----------------------------------------------
-PROP.NEGS = c(0.1, 0.2, 0.3)
-
-##PROP.NEGS = NA # DO NOT DEFINE FOR NOW
-# Note that PROP.NEGS should be 'NA' when DENSITY=1.0
-# In the point of view of a developer, if PROP.NEGS = NA, we should know that prop.neg can be only 1 value and it can be computed from (graph.size, prop.mispl)
-# Otherwise, user should be able to give a common range of prop.negs for each pair of (graph.size, prop.mispl)
-# ----------------------------------------------
-
-INPUT.RANDOM.NETWORKS = seq(1, 2, by=1)
-# INPUT.RANDOM.NETWORKS = seq(1, 10, by=1)
-# --------------------------------------------------------------
+##########################################################################
+# Another scenario: This is used in the article
+##########################################################################
+#GRAPH.SIZES = seq(16, 36, by=4)
+#PROP.MISPLS = c( seq(0.05, 0.40, by=0.05)) # when l0=4 is selected
+#PROP.MISPLS = c( seq(0.05, 0.60, by=0.05)) # when l0=3 is selected
+#PROP.MISPLS = c( seq(0.05, 0.85, by=0.05) ) # when l0=2 is selected
+#DENSITY = 1 # we can only give a single value (not a vector !!)
+#L0 = 2 # OR L0 = 3 OR L0 = 4
+#PROP.NEGS = NA
+#INPUT.RANDOM.NETWORKS = seq(1, 100, by=1)
+# =======================================================================
 
 
 
 
-# ====================================
-# TODO
-# Likewise, the code works for the signed graphs with integer weights ?
-# TODO: for(graph.desc.name in c(SIGNED.UNWEIGHTED.FILE)){ # SIGNED.WEIGHTED.FILE
-#           ..... ==> boyle olmasin, main.R'de bunlari variable'a al
-#
-# - it the responsability of the user who will ensure if the RAM requirement of his/her system is ok for large graphs,
-#     since Cplex may require a lot of RAM for graphs whose size is larger than 28
-# - scale of y-axis should not be normalized for VI (or for another unnormalized measure)
-# - transition graph ? plots?
-# - opposite networks ? I think, it is another subject/work
-# - the choice of internal evaluation measure is not possible for now. We use onyl Silhouette measure.
-#       Later, we can use other measures, if needed. But, the code needs to be changed a bit.
-# - tracking the used memory during Cplex is probably system dependent, i.e. only Linux ..
-# - manage the output of ExCC:
-#   - TODO: for Cplex output, we have the following Java warning: In readLines(con) : incomplete final line found on './out/partitions/../sol49.txt'
-# - the whole scripts are run for all types of input graphs ? like SIGNED.WEIGHTED.FILE ? TODO
-#       But, it is sure that directed signed graphs are not supported
-# TODO: in compare-partitions.R, handle better the global variables in clusters
-# TODO: in 'evaluate-partitions.R', you can also put the used RAM statistic as a output csv file
-# TODO: Actually, prop.neg can be computed without knowing prop.mispl when d=1. You may simplify some process in layout plot prcoess?
-#
-# Remark: CORE.PART.THRESHOLD should be set to 1, not less than 1. Because, when it is less than 1, there might be multiple way of showing core part.
-# ====================================
 
 
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
 
 #################################################################
-# POST-PROCESSING INPUT NETWORKS ==> since layout algos are stochastic, the obtained layout is used whenever plot is used
+# POST-PROCESSING INPUT NETWORKS ==> since layout algos are stochastic,
+#       the obtained layout is used whenever plot is used
 #################################################################
-add.layouts.for.all.networks(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+add.layouts.for.all.networks(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
                     UNSIGNED.GRAPH.LAYOUTS, SIGNED.GRAPH.LAYOUTS)
-
 
 
 ##################################################################
 # PARTITION NETWORKS
 ##################################################################
-partition.networks(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+partition.networks(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 	                COR.CLU.EXACT.ALGO, keep.algo.log.files, plot.format, plot.layout, FORCE)
 
 
 #################################################################
-# CREATE GEPHI NETWORKS with partition info for all solutions ==> it is a bit slower (depending on the number of obtained partitions)
+# CREATE GEPHI NETWORKS with partition info for all solutions
+#   ==> it is a bit slower (depending on the number of obtained partitions)
 #################################################################
-create.gephi.networks(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS, COR.CLU.EXACT.ALGO, FORCE)
-
-
-
+create.gephi.networks(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS, COR.CLU.EXACT.ALGO, FORCE)
 
 
 
 #################################################################
 # EVALUATE PARTITIONS
 #################################################################
-evaluate.partitions(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+evaluate.partitions(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
                     COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE)
-
-
 
 
 #################################################################
 # CLUSTER ANALYSIS
 #################################################################
 K.LIMITS = c(1,20)
-perform.all.cluster.analysis(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+perform.all.cluster.analysis(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
         COR.CLU.EXACT.ALGO, COMP.MEASURES, K.LIMITS, FORCE)
 
 
 # # ################################################################
 # # # EVALUATE PARTITIONS WITH KMEDOID
 # # ################################################################
-evaluate.partitions.with.kmedoid.results(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+evaluate.partitions.with.kmedoid.results(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
         COR.CLU.EXACT.ALGO,
         #VI,
         COMP.MEASURES,
@@ -192,7 +157,7 @@ evaluate.partitions.with.kmedoid.results(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, P
 # ##############################################################
 # # CLUSTER CHARACTERIZATION
 # ##############################################################
-perform.all.cluster.characterization(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+perform.all.cluster.characterization(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
             COR.CLU.EXACT.ALGO, COMP.MEASURES, K.LIMITS, FORCE)
 
 
@@ -203,14 +168,14 @@ perform.all.cluster.characterization(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.
 ##     NMI,
 ##     VI
 ## )
-# show.all.cluster.analysis.difference(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+# show.all.cluster.analysis.difference(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 #         COR.CLU.EXACT.ALGO, COMP.MEASURES, K.LIMITS, FORCE)
 
 
 # ##################################################################
 # ## EVALUATE PARTITIONS WITH CORE PART ANALYSIS AND REPRESENTATIVES
 # ##################################################################
-evaluate.partitions.with.cluster.characterization(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+evaluate.partitions.with.cluster.characterization(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
                 COR.CLU.EXACT.ALGO,
                 #VI,
                 COMP.MEASURES,
@@ -218,20 +183,15 @@ evaluate.partitions.with.cluster.characterization(GRAPH.SIZES, DENSITY, K, PROP.
 
 
 
-
-
-
-
 ##################################################################
 ## REORGANIZE
 ##################################################################
-reorganize.all.csv.results.by.detected.imbalance(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+reorganize.all.csv.results.by.detected.imbalance(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
                                               COR.CLU.EXACT.ALGO, FORCE)
 
 # # for proportion of misplaced AND detected imbalance intervals
-reorganize.all.csv.results.by.sol.class(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+reorganize.all.csv.results.by.sol.class(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
                                              COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE)
-
 
 
 
@@ -239,10 +199,8 @@ reorganize.all.csv.results.by.sol.class(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DE
 ### COMBINE EVALUATED PARTITIONS
 ###################################################################
 ## for proportion of misplaced  AND  detected imbalance intervals
-take.average.over.networks(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+take.average.over.networks(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
         COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE)
-
-
 
 
 
@@ -251,20 +209,20 @@ take.average.over.networks(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.IN
 ##################################################################
 ## LAYOUT PLOTS
 ##################################################################
-make.layout.plots.by.graph.size.and.imbalance(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+make.layout.plots.by.graph.size.and.imbalance(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 	COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
-make.layout.plots.by.graph.size.and.network.no(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+make.layout.plots.by.graph.size.and.network.no(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 	COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
-make.layout.plots.by.imbalance.and.network.no(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+make.layout.plots.by.imbalance.and.network.no(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 	COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
 
 ###################################################################
 ### SUMMARY FOLDER - LAYOUT PLOTS
 ###################################################################
-make.layout.plots.by.graph.size.and.imbalance(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, c(SUMMARY.FOLDER.NAME),
+make.layout.plots.by.graph.size.and.imbalance(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, DETECTED.IMB.INTERVALS, PROP.NEGS, c(SUMMARY.FOLDER.NAME),
 	 COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
 
@@ -275,12 +233,41 @@ make.layout.plots.by.graph.size.and.imbalance(GRAPH.SIZES, DENSITY, K, PROP.MISP
 # WRITE SOME RESULTS INTO CSV FILES
 #################################################################
 
-collect.all.best.k.for.kmedoids(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+collect.all.best.k.for.kmedoids(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 		COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
-collect.all.nb.opt.solution(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+collect.all.nb.opt.solution(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 		COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
 
-collect.core.parts(GRAPH.SIZES, DENSITY, K, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
+collect.core.parts(GRAPH.SIZES, DENSITY, L0, PROP.MISPLS, PROP.NEGS, INPUT.RANDOM.NETWORKS,
 	COR.CLU.EXACT.ALGO, COMP.MEASURES, FORCE, plot.format)
+
+
+
+
+
+
+
+# ====================================
+# TODOs
+# ====================================
+#
+# - the code works for the signed graphs with integer weights ?
+# - for(graph.desc.name in c(SIGNED.UNWEIGHTED.FILE)){ # SIGNED.WEIGHTED.FILE
+#       * do not do in that way, put the potetial values into global list in main.R
+#       * In folder hierarchy, we need to add another child folder called "signed-unweighted" in in/random-networks/...
+#
+# - scale of y-axis should not be normalized for VI (or for another unnormalized measure)
+# - the choice of internal evaluation measure is not possible for now. We use only Silhouette measure.
+#       * Later, we can use other measures, if needed. But, the code needs to be changed a bit.
+# - tracking the used memory during Cplex is probably system dependent, i.e. only Linux ..
+# - manage the output of ExCC:
+# - for Cplex output, we have the following Java warning: In readLines(con) : incomplete final line found on './out/partitions/../sol49.txt'
+# - the whole scripts are run for all types of input graphs ? like SIGNED.WEIGHTED.FILE ?
+#       * But, it is sure that directed signed graphs are not supported
+# - in compare-partitions.R, handle better the global variables in clusters
+# - in 'evaluate-partitions.R', you can also put the used RAM statistic as a output csv file
+# - Actually, prop.neg can be computed without knowing prop.mispl when d=1. You may simplify some process in layout plot prcoess?
+#
+# # ====================================
 
